1b6106da1c7d249bfd4bf87c83ddd0967c875cac
JoseLu-Dev <joseludev@gmail.com>
2022-11-27 17:07:51 +0100

    feat: :sparkles: Create and move files

---
+++ src/common/useCases/writeComputedFiles.ts
@@ -0,0 +1,33 @@
+import { Service } from "typedi";
+
+import { FileWrapper } from "../fileWrapper"
+import { CreateDir } from "../fs/write/createDir";
+import { CreateFile } from "../fs/write/createFile";
+import { MoveFile } from "../fs/write/moveFile";
+
+@Service()
+export class WriteComputedFiles {
+
+    constructor(
+        private readonly _createDir: CreateDir,
+        private readonly _createFile: CreateFile,
+        private readonly _moveFile: MoveFile,
+    ) { }
+
+
+    async write(files: Array<FileWrapper>): Promise<void[]> {
+        return Promise.all(files.map(file => this._performFileWrite(file)))
+    }
+
+    private async _performFileWrite(file: FileWrapper): Promise<void> {
+        if (!file.isNew)
+            return this._moveFile.move(file.pathCurrentComplete(), file.pathNewComplete())
+
+        if (file?.stats?.isFile())
+            return this._createFile.create(file.pathNewComplete())
+
+        if (file?.stats?.isDirectory())
+            return this._createDir.create(file.pathNewComplete())
+    }
+
+}
---
+++ tests/integration/common/useCases/writeComputedFiles.test.ts
@@ -0,0 +1,64 @@
+import 'reflect-metadata'
+
+const withLocalTmpDir = require('with-local-tmp-dir')
+const outputFiles = require('output-files')
+const dirTree = require("directory-tree");
+import { Container } from 'typedi'
+import { FileWrapper } from '../../../../src/common/fileWrapper'
+
+import { WriteComputedFiles } from '../../../../src/common/useCases/writeComputedFiles'
+
+const writeComputedFiles: WriteComputedFiles = Container.get(WriteComputedFiles)
+
+const folderStructure = {
+    'video.mp4': '',
+}
+
+describe('write', () => {
+
+    it('modifies or creates folder and file structure', () => withLocalTmpDir(async () => {
+        await outputFiles(folderStructure)
+
+        const files: FileWrapper[] = [
+            {
+                pathCurrentComplete: () => `${process.cwd()}\\video.mp4`,
+                pathNewComplete: () => `${process.cwd()}\\videos\\video.mp4`
+            },
+            {
+                pathNewComplete: () => `${process.cwd()}\\newFile.file`,
+                isNew: true,
+                stats: {
+                    isFile: () => true
+                }
+            },
+            {
+                pathNewComplete: () => `${process.cwd()}\\newFolder`,
+                isNew: true,
+                stats: {
+                    isDirectory: () => true,
+                    isFile: () => false,
+                }
+            }
+        ] as FileWrapper[]
+        
+        
+        await writeComputedFiles.write(files)
+        
+
+        const tree = dirTree(process.cwd(), {attributes: ['type']});
+
+        const treeFiles = tree.children
+
+        const fileMoved = treeFiles.filter(c => c.name === 'videos')[0].children[0]
+        expect(fileMoved.name).toBe('video.mp4')
+
+        const fileCreated = treeFiles.filter(c => c.name === 'newFile.file')[0]
+        expect(fileCreated.name).toBe('newFile.file')
+        expect(fileCreated.type).toBe('file')
+
+        const folderCreated = treeFiles.filter(c => c.name === 'newFolder')[0]
+        expect(folderCreated.name).toBe('newFolder')
+        expect(folderCreated.type).toBe('directory')
+    }))
+
+})
